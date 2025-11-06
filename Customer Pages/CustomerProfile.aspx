37ab53c92d63075f44c9fc590803fc419f5de8c3
aisha-pdf <abbaomaraisha@gmail.com>
2025-11-06 12:40:10 +0200

    update customer profile - added address update

--- Customer Pages/CustomerProfile.aspx
+++ Customer Pages/CustomerProfile.aspx
@@ -82,12 +82,18 @@
             <div class="col-md-6">
                 <h5>Address Details</h5>
                 <hr />
-                <asp:DetailsView ID="DetailsView1" runat="server" AutoGenerateRows="False" DefaultMode="Edit" DataSourceID="DSAddress" Width="100%" OnItemCommand="DetailsView1_ItemCommand" BorderColor="White">
-                    <HeaderStyle CssClass="p-2" />
+                <asp:DetailsView ID="DetailsView1" runat="server" AutoGenerateRows="False" DefaultMode="Edit" DataSourceID="DSAddress"
+                    BackColor="White" BorderColor="#CCCCCC" BorderStyle="None" BorderWidth="1px" CellPadding="4"
+                    ForeColor="Black" GridLines="None" Width="100%" OnItemCommand="DetailsView1_ItemCommand"
+                    OnItemUpdating="DetailsView1_ItemUpdating" OnItemUpdated="DetailsView1_ItemUpdated" Height="156px">
+
+                   
+                    <HeaderStyle BackColor="#333333" Font-Bold="True" ForeColor="White" CssClass="p-2" />
+                    <FooterStyle BackColor="#CCCC99" ForeColor="Black" />
+                    <PagerStyle BackColor="White" ForeColor="Black" HorizontalAlign="Right" />
 
                     <Fields>
-
-                        <asp:TemplateField HeaderText="Street Address:">
+                        <asp:TemplateField HeaderText="Street Address">
                             <EditItemTemplate>
                                 <div class="form-group mb-3">
                                     <asp:TextBox ID="txtStreetAddress" runat="server" CssClass="form-control"
@@ -98,9 +104,7 @@
                                 <%# Eval("streetAddress") %>
                             </ItemTemplate>
                         </asp:TemplateField>
-
-
-                        <asp:TemplateField HeaderText="Suburb:">
+                        <asp:TemplateField HeaderText="Suburb">
                             <EditItemTemplate>
                                 <div class="form-group mb-3">
                                     <asp:TextBox ID="txtSuburb" runat="server" CssClass="form-control"
@@ -113,7 +117,7 @@
                         </asp:TemplateField>
 
 
-                        <asp:TemplateField HeaderText="Postal Code:">
+                        <asp:TemplateField HeaderText="Post Code">
                             <EditItemTemplate>
                                 <div class="form-group mb-3">
                                     <asp:TextBox ID="txtPostCode" runat="server" CssClass="form-control"
@@ -124,72 +128,89 @@
                                 <%# Eval("postCode") %>
                             </ItemTemplate>
                         </asp:TemplateField>
-
+                        <asp:TemplateField HeaderText="Phone Number" Visible="False">
+                            <ItemTemplate>
+                                <asp:Label ID="lblPhoneNum" runat="server"
+                                    Text='<%# Eval("phoneNum") %>'
+                                    ForeColor="White" Font-Bold="true" CssClass="p-1 rounded" />
+                            </ItemTemplate>
+                        </asp:TemplateField>
 
                         <asp:TemplateField>
                             <EditItemTemplate>
                                 <div class="form-group mt-4 d-flex gap-2">
                                     <asp:Button ID="btnUpdate" runat="server" CommandName="Update" Text="Save Changes"
                                         CssClass="menu-btn" />
-                                    <asp:Button ID="btnCancel" runat="server" CommandName="Cancel" Text="Cancel"
+                                    <asp:Button ID="btnCancel" runat="server" CommandName="CancelChanges" Text="Cancel"
                                         CssClass="menu-btn" />
                                 </div>
                             </EditItemTemplate>
                         </asp:TemplateField>
                     </Fields>
                 </asp:DetailsView>
+
+                <%-- <asp:DetailsView ID="DetailsView1" runat="server" AutoGenerateRows="False" DefaultMode="Edit" DataSourceID="DSAddress" Width="100%" OnItemCommand="DetailsView1_ItemCommand" BorderColor="White">
+                    <HeaderStyle CssClass="p-2" />
+                    <Fields>
+                        <asp:BoundField DataField="streetAddress" HeaderText="streetAddress" SortExpression="streetAddress" />
+                        <asp:BoundField DataField="suburb" HeaderText="suburb" SortExpression="suburb" />
+                        <asp:BoundField DataField="postCode" HeaderText="postCode" SortExpression="postCode" />
+                        
+                    </Fields>
+                </asp:DetailsView>
+            </div>--%>
+
+                <asp:SqlDataSource ID="DSAddress" runat="server" ConnectionString="<%$ ConnectionStrings:LoginConnectionString %>" SelectCommand="SELECT streetAddress, suburb, postCode, phoneNum FROM Customer WHERE (customerID = @CusID)" UpdateCommand="UPDATE Customer SET streetAddress = @streetAddress, suburb = @suburb, postCode = @postCode, phoneNum = @phoneNum WHERE (customerID = @CusID)">
+                    <SelectParameters>
+                        <asp:Parameter Name="CusID" />
+                    </SelectParameters>
+                    <UpdateParameters>
+                        <asp:Parameter Name="streetAddress" />
+                        <asp:Parameter Name="suburb" />
+                        <asp:Parameter Name="postCode" />
+                        <asp:Parameter Name="phoneNum" />
+                        <asp:Parameter Name="CusID" />
+                    </UpdateParameters>
+                </asp:SqlDataSource>
             </div>
 
-            <asp:SqlDataSource ID="DSAddress" runat="server" ConnectionString="<%$ ConnectionStrings:LoginConnectionString %>" SelectCommand="SELECT streetAddress, suburb, postCode FROM Customer WHERE (customerID = @CusID)" UpdateCommand="UPDATE Customer SET streetAddress = @streetAddress, suburb = @suburb, postCode = @postCode WHERE (customerID = @CusID)">
-                <SelectParameters>
-                    <asp:Parameter Name="CusID" />
-                </SelectParameters>
-                <UpdateParameters>
-                    <asp:Parameter Name="streetAddress" />
-                    <asp:Parameter Name="suburb" />
-                    <asp:Parameter Name="postCode" />
-                    <asp:Parameter Name="CusID" />
-                </UpdateParameters>
-            </asp:SqlDataSource>
+            <!-- Styles for Buttons -->
+            <style>
+                .button-group {
+                    display: flex;
+                    gap: 10px; /* space between buttons */
+                }
+
+                .menu-btn {
+                    background: #6C4F85;
+                    color: white;
+                    border: none;
+                    padding: 10px;
+                    text-align: left;
+                    border-radius: 5px;
+                    cursor: pointer;
+                    width: 100%;
+                    font-size: 14px;
+                }
+
+                    .menu-btn:hover {
+                        background: #7D5C99;
+                    }
+
+                .action-btn {
+                    background: #6C4F85;
+                    color: white;
+                    border: none;
+                    padding: 8px 15px;
+                    border-radius: 6px;
+                    margin-left: 5px;
+                    cursor: pointer;
+                }
+
+                    .action-btn:hover {
+                        background: #8365A8;
+                    }
+            </style>
         </div>
-
-        <!-- Styles for Buttons -->
-        <style>
-            .button-group {
-                display: flex;
-                gap: 10px; /* space between buttons */
-            }
-
-            .menu-btn {
-                background: #6C4F85;
-                color: white;
-                border: none;
-                padding: 10px;
-                text-align: left;
-                border-radius: 5px;
-                cursor: pointer;
-                width: 100%;
-                font-size: 14px;
-            }
-
-                .menu-btn:hover {
-                    background: #7D5C99;
-                }
-
-            .action-btn {
-                background: #6C4F85;
-                color: white;
-                border: none;
-                padding: 8px 15px;
-                border-radius: 6px;
-                margin-left: 5px;
-                cursor: pointer;
-            }
-
-                .action-btn:hover {
-                    background: #8365A8;
-                }
-        </style>
-    </div>
     </div>
 </asp:Content>
--- Customer Pages/CustomerProfile.aspx.cs
+++ Customer Pages/CustomerProfile.aspx.cs
@@ -11,6 +11,7 @@ namespace TroikaClothingWeb.Customer_Pages
 {
     public partial class CustomerProfile : System.Web.UI.Page
     {
+        string phoneNo = "";
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -22,6 +23,7 @@ namespace TroikaClothingWeb.Customer_Pages
                 {
                     DSWebDetails.DataBind();
                     this.DataBind(); // fills textboxes with data
+                    
                 }
                 else
                 {
@@ -29,14 +31,67 @@ namespace TroikaClothingWeb.Customer_Pages
                 }
             }
 
+            if (CustomerForm.CurrentMode == FormViewMode.Edit)
+            {
+                // Try to get the phone number textbox from the FormView
+                TextBox phoneNum = (TextBox)CustomerForm.FindControl("PhoneNumber");
+
+                if (phoneNum != null)
+                {
+                    string phoneNo = phoneNum.Text.Trim();
+
+                    // Try to get the label from the DetailsView
+                    Label lblPhone = (Label)DetailsView1.FindControl("lblPhoneNum");
+                    if (lblPhone != null)
+                    {
+                        lblPhone.Text = phoneNo;
+                    }
+                }
+            }
+
+            //TextBox phoneNum = (TextBox)CustomerForm.FindControl("PhoneNumber");
+            //if (phoneNum == null && CustomerForm.CurrentMode == FormViewMode.Edit)
+            //{
+            //    phoneNum = (TextBox)CustomerForm.Row.FindControl("PhoneNumber");
+            //    phoneNo = phoneNum.Text;
+            //    Label lblPhone = (Label)DetailsView1.FindControl("lblPhoneNumber");
+
+
+            //    if (lblPhone == null)
+            //    {
+            //        lblPhone = (Label)DetailsView1.Rows[0].FindControl("lblPhoneNumber");
+            //        lblPhone.Text = phoneNo;
+            //    }
+
+            //}
+
+
             string customerID = GetCustomerIDByUsername(username);
-            //lblCusID.Text = customerID;
             DSAddress.SelectParameters["CusID"].DefaultValue = customerID;
             DSAddress.UpdateParameters["CusID"].DefaultValue = customerID;
-            //fvAddress.DataBind();
-
+           
         }
 
+        //protected void Page_PreRender(object sender, EventArgs e)
+        //{
+        //    if (CustomerForm.CurrentMode == FormViewMode.Edit)
+        //    {
+        //        // Try to get the phone number textbox from the FormView
+        //        TextBox phoneNum = (TextBox)CustomerForm.FindControl("PhoneNumber");
+
+        //        if (phoneNum != null)
+        //        {
+        //            string phoneNo = phoneNum.Text.Trim();
+
+        //            // Try to get the label from the DetailsView
+        //            Label lblPhone = (Label)DetailsView1.FindControl("lblPhoneNum");
+        //            if (lblPhone != null)
+        //            {
+        //                lblPhone.Text = phoneNo;
+        //            }
+        //        }
+        //    }
+        //}
 
 
         private string GetCustomerIDByUsername(string username)
@@ -86,61 +141,141 @@ namespace TroikaClothingWeb.Customer_Pages
         {
             try
             {
-                // perform update and display a success message
-                CustomerForm.UpdateItem(true); 
+                // 1️⃣ Get the phone number textbox from the FormView
+                TextBox txtPhone = (TextBox)CustomerForm.FindControl("PhoneNumber");
+                string newPhone = txtPhone != null ? txtPhone.Text.Trim() : "";
+
+                // 2️⃣ Update the FormView data
+                CustomerForm.UpdateItem(true);
+
+                // 3️⃣ Try to find the phone number label inside DetailsView
+                Label lblPhone = (Label)DetailsView1.FindControl("lblPhoneNumber");
+                if (lblPhone != null)
+                {
+                    lblPhone.Text = newPhone;  // update it manually
+                }
+                else
+                {
+                    // For debugging: helps if the label can’t be found
+                    LblMessage.ForeColor = System.Drawing.Color.Red;
+                    LblMessage.Text = "Warning: Phone label not found in DetailsView.";
+                }
+
+                // 4️⃣ Display success message
                 LblMessage.ForeColor = System.Drawing.Color.Green;
-                LblMessage.Text = "Update successful!";
+                LblMessage.Text = "Your details have been updated successfully.";
             }
             catch (Exception ex)
             {
-                // show error message
+                // 5️⃣ Show error if something fails
                 LblMessage.ForeColor = System.Drawing.Color.Red;
                 LblMessage.Text = "Error updating record: " + ex.Message;
-
-                // cancel changes made
                 e.Cancel = true;
             }
         }
 
+        //protected void CustomerForm_ItemUpdating(object sender, FormViewUpdateEventArgs e)
+        //{
+        //    try
+        //    {
+        //        // perform update and display a success message
+        //        CustomerForm.UpdateItem(true);
+
+        //        //updating phonenumber label 
+        //        TextBox phoneNum = (TextBox)CustomerForm.FindControl("PhoneNumber");
+        //        phoneNo = phoneNum.Text;
+        //        Label lblPhone = (Label)DetailsView1.FindControl("lblPhoneNumber");
+        //        lblPhone.Text = phoneNo;
+
+        //        //show success message
+        //        LblMessage.ForeColor = System.Drawing.Color.Green;
+        //        LblMessage.Text = "Your details have been updated";
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        // show error message
+        //        LblMessage.ForeColor = System.Drawing.Color.Red;
+        //        LblMessage.Text = "Error updating record: " + ex.Message;
+
+        //        // cancel changes made
+        //        e.Cancel = true;
+        //    }
+        //}
+
         protected void DetailsView1_ItemCommand(object sender, DetailsViewCommandEventArgs e)
         {
-            if (e.CommandName == "Cancel")
+            if (e.CommandName == "CancelChanges")
             {
                 DetailsView1.ChangeMode(DetailsViewMode.Edit);
                 DetailsView1.DataBind();
             }
         }
 
-        //protected void fvAddress_ItemCommand(object sender, FormViewCommandEventArgs e)
-        //{
-        //    //if cancelled button is clicked reload data
-        //    if (e.CommandName == "Cancel")
-        //    {
-        //        CustomerForm.ChangeMode(FormViewMode.Edit);
-        //        CustomerForm.DataBind();
-        //    }
-        //}
+        protected void DetailsView1_ItemUpdated(object sender, DetailsViewUpdatedEventArgs e)
+        {
+            try
+            {
+                // if update is successful
+                LblMessage.ForeColor = System.Drawing.Color.Green;
+                LblMessage.Text = "Address has been updated";
+            }
+            catch (Exception ex)
+            {
+                // show error message
+                LblMessage.ForeColor = System.Drawing.Color.Red;
+                LblMessage.Text = "Error updating record: " + ex.Message;
+            }
+        }
 
-        //protected void fvAddress_ItemUpdating(object sender, FormViewUpdateEventArgs e)
-        //{
-        //    string username = Session["Username"]?.ToString();
-        //    string customerID = GetCustomerIDByUsername(username);
-        //    DSAddress.UpdateParameters["CusID"].DefaultValue = customerID;
-        //}
+        protected void DetailsView1_ItemUpdating(object sender, DetailsViewUpdateEventArgs e)
+        {
+            try
+            {
+                // getting data from text boxes
+                DetailsView dv = (DetailsView)sender;
+                TextBox txtStreet = (TextBox)dv.FindControl("txtStreetAddress");
+                TextBox txtSuburb = (TextBox)dv.FindControl("txtSuburb");
+                TextBox txtPostCode = (TextBox)dv.FindControl("txtPostCode");
 
-        //protected void fvAddress_ItemUpdated(object sender, FormViewUpdatedEventArgs e)
-        //{
-        //    if (e.Exception != null)
-        //    {
-        //        LblMessage.ForeColor = System.Drawing.Color.Red;
-        //        LblMessage.Text = "Error updating address: " + e.Exception.Message;
-        //        e.ExceptionHandled = true;
-        //    }
-        //    else
-        //    {
-        //        LblMessage.ForeColor = System.Drawing.Color.Green;
-        //        LblMessage.Text = "Address updated successfully!";
-        //    }
-        //}
+                // validating street address
+                if (string.IsNullOrWhiteSpace(txtStreet.Text))
+                {
+                    e.Cancel = true;
+                    LblMessage.ForeColor = System.Drawing.Color.Red;
+                    LblMessage.Text = "Please fill in your street address";
+                    return;
+                }
+
+                // validating suburb
+                if (string.IsNullOrWhiteSpace(txtSuburb.Text))
+                {
+                    e.Cancel = true;
+                    LblMessage.ForeColor = System.Drawing.Color.Red;
+                    LblMessage.Text = "Please fill in your suburb";
+                    return;
+                }
+
+
+                // validating postal code
+                if (string.IsNullOrWhiteSpace(txtPostCode.Text) || txtPostCode.Text.Length != 4)
+                {
+                    e.Cancel = true;
+                    LblMessage.ForeColor = System.Drawing.Color.Red;
+                    LblMessage.Text = "Please fill in your postal code";
+                    return;
+                }
+
+
+                // Optional success message before update
+                LblMessage.ForeColor = System.Drawing.Color.Green;
+                LblMessage.Text = "Updating your address...";
+            }
+            catch (Exception ex)
+            {
+                e.Cancel = true; // Stop the update if something fails
+                LblMessage.ForeColor = System.Drawing.Color.Red;
+                LblMessage.Text = "Error while updating: " + ex.Message;
+            }
+        }
     }
 }
